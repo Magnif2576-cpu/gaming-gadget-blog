a81ce197c867058269a815a5abc95893fb731929
Magni <yuudai2576@gmail.com>
2025-09-24 20:50:53 +0900

    add google-site-verification via head.tsx

---
+++ src/app/head.tsx
@@ -0,0 +1,11 @@
+// src/app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="zcYcSQ1xt1oXvPra_qnhFQf2ART9uxUKIZYiPpGF5Ig" // ← GSC の値
+      />
+    </>
+  );
+}
